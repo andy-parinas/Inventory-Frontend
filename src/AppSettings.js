19a1a05f80003c3175814a5003aa53e1cc9ff704
Andy Parinas <andy.parinas@gmail.com>
2018-10-26 11:08:35 +1000

    Change API url for testing

--- src/AppSettings.js
+++ src/AppSettings.js
@@ -1,1 +1,2 @@
-export const InventoryBackendAPI = 'http://localhost:5000/api';
+// export const InventoryBackendAPI = 'http://localhost:5000/api'; //For Development Only
+export const InventoryBackendAPI = 'http://local.testsever/api'; //For Testing Purposes

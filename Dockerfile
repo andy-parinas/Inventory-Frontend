5da0d032234d13239506a680e38577b6d6cdad2a
Andy Parinas <andy.parinas@gmail.com>
2018-11-07 21:26:16 +1000

    added css compress.

--- Dockerfile
+++ Dockerfile
@@ -9,9 +9,11 @@ RUN npm install --silent
 RUN npm install react-scripts -g --silent
 RUN npm run build
 RUN npm run build-css
+RUN npm run compress-css
 
 ### STAGE 2: Production Environment ###
 FROM nginx:1.13.12-alpine
 COPY --from=build /usr/src/app/build /usr/share/nginx/html
+COPY nginx.conf /etc/nginx/nginx.conf
 EXPOSE 80
 CMD ["nginx", "-g", "daemon off;"]
--- src/AppSettings.js
+++ src/AppSettings.js
@@ -1,2 +1,2 @@
-export const InventoryBackendAPI = 'http://localhost:5000/api'; //For Development Only
-// export const InventoryBackendAPI = 'http://local.testsever/api'; //For Testing Purposes
+// export const InventoryBackendAPI = 'http://localhost:5000/api'; //For Development Only
+export const InventoryBackendAPI = 'http://api.inventorymanagement.com/api'; //For Testing Purposes

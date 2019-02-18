7bdb2a2f316e8e41b078275f28e1d5fe50120fed
Andy Parinas <andy.parinas@gmail.com>
2019-02-18 10:51:34 +1000

    Updated the api url

--- src/AppSettings.js
+++ src/AppSettings.js
@@ -1,3 +1,4 @@
 // export const InventoryBackendAPI = 'http://localhost:5000/api'; //For Development Only
 // export const InventoryBackendAPI = 'http://api.inventorymanagement.test/api'; //For Testing Purposes
-export const InventoryBackendAPI = 'http://api1.ctplus.com.ph/api'; //Production URL
+// export const InventoryBackendAPI = 'http://api1.ctplus.com.ph/api'; //Production URL
+export const InventoryBackendAPI = 'http://api.ctplus.com.au/api'; //Production URL
